1d72c93445a58b791b7420b0904d4165f47716ea
Егор Пигалёв <1pigaleve@mail.ru>
2022-11-30 14:20:24 +0300

    Добавлена БД в проект

---
+++ Pigalev_travel_around_russia/classes/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pigalev_travel_around_russia
+{
+    class Base
+    {
+        public static BaseData BE;
+    }
+}
