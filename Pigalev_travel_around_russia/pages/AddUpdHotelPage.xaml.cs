eccad716028ccf3866dd728f959a75bc02d767d0
EgorPigalev <1pigaleve@mail.ru>
2022-12-03 22:11:02 +0300

    Доработки формы добавления/изменения отеля

--- Pigalev_travel_around_russia/pages/AddUpdHotelPage.xaml.cs
+++ Pigalev_travel_around_russia/pages/AddUpdHotelPage.xaml.cs
@@ -20,18 +20,44 @@ namespace Pigalev_travel_around_russia
     /// </summary>
     public partial class AddUpdHotelPage : Page
     {
-        public AddUpdHotelPage()
+        Hotel hotel;
+        bool flagUpdate = false;
+        private void UploadFields()
+        {
+            cbCountry.ItemsSource = Base.BE.Country.ToList();
+            cbCountry.SelectedValuePath = "Code";
+            cbCountry.DisplayMemberPath = "Name";
+            cbCountry.SelectedIndex = 0;
+        }
+            public AddUpdHotelPage()
         {
             InitializeComponent();
+            UploadFields();
+
         }
         public AddUpdHotelPage(Hotel hotel)
         {
             InitializeComponent();
+            UploadFields();
+            this.hotel = hotel;
+            flagUpdate = true;
             tbHeader.Text = "Изменение существующего отеля";
+            btnAddUpd.Content = "Измененить отель";
+            tbName.Text = hotel.Name;
+            tbCountOfStars.Text = Convert.ToString(hotel.CountOfStars);
+            cbCountry.SelectedValue = hotel.CountryCode;
         }
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
             FrameClass.MainFrame.Navigate(new HotelsPage());
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if(!((e.Text == "0") || (e.Text == "1") || (e.Text == "2") || (e.Text == "3") || (e.Text == "4") || (e.Text == "5")))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
